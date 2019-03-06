75e0230cc941163f5b3c916a8bcbf059e776ba3b
caseysmithrc <30840394+caseysmithrc@users.noreply.github.com>
2019-03-06 05:46:24 -0700

    Initial Commit

---
+++ Loader/Loader.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+public class TestClass
+{
+    public TestClass()
+    {
+        
+    }
+
+    public static string RunProcess(string path)
+    {
+        Process.Start("calc.exe");
+        return "boom!";
+    }
+}
